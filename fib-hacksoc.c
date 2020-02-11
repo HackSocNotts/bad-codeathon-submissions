5d75ffb57325703431dbf9bdf5d20ba678188008
Nathan Brechard <psynb4@linode001.cs.nott.ac.uk>
2020-02-11 21:29:07 +0000

    file

---
+++ fib-hacksoc.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc,  char ** argv){ int xxxxxxx = 3; int xx; int x = 1; int xxxx = 1; int xxx; int xxxxx;
+    printf("E");  printf("n");  printf("t");  printf("e"); 
+                    printf("r");   printf(" ");         printf("t");    printf("h");   
+                            printf("e"); printf(" ");  
+     printf("d"); printf("e");  printf("s");  printf("i");  printf("r");
+                    printf("e");   printf("d"); printf(" ");  printf("v");           
+printf("a");   printf("l");
+        printf("u");  printf("e");
+                                            printf("d\n"); scanf("%d", &xx);
+                                        
+                                                                                                    xxx = 0;
+                xxxxxx: do{
+                            if (xxx <= 1){ xxxxx = xxx; xxx = xxx + 1; xxxxxxx = xxxxxxx + 1; goto xxxxxx;
+            }                                                                                               
+                                                                                                                    if (xxxxxxx == 3){
+                xxxxx = x + xxxx;
+                                                                     x = xxxx; xxxx = xxxxx;                  
+     xxxxxxx = xxxxxxx + 1;                    xxx = xxx + 1;
+    }
+    else if (xxxxxxx == 4){
+                            xxxxx = x + xxxx;
+                                                        x = xxxx;
+                xxxx = xxxxx;
+    
+     xxxxxxx = xxxxxxx + 1; xx = xxx + 1;
+    }
+    else if (xxxxxxx == 5){
+                                        xxxxx = x + xxxx;
+                                                             x = xxxx;
+                                                                                                                                                 xxxx = xxxxx;
+                                                                        xxxxxxx = xxxxxxx + 1; xxx = xxx + 1;
+    }
+    else if (xxxxxxx == 6){
+                            xxxxx = x + xxxx;
+            x = xxxx;
+                 xxxx = xxxxx;
+     xxxxxxx = xxxxxxx + 1;                                 
+                xxx = xxx + 1;
+    }
+     else if (xxxxxxx == 7){
+                            xxxxx = x + xxxx;
+            x = xxxx;
+                 xxxx = xxxxx;
+     xxxxxxx = xxxxxxx + 1;                                 
+                xxx = xxx + 1;
+    }
+    else 
+    {
+            xxxxx = x + xxxx;
+    x = xxxx;
+                    xxxx = xxxxx;
+    
+     xxxxxxx = xxxxxxx + 1;                xxx = xxx + 1;
+    }
+    }while (xxx < (xx - 1));
+                                printf("%d\n", xxxxx);
+                                                                                        return 0;}
+
