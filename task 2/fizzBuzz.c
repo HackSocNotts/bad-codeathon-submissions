38ec72ed42d6c27c51f58db0044aeec1173d298f
Maximilian Zielonka <psymz@nottingham.ac.uk>
2020-02-11 21:06:12 +0000

    best formatting

--- task 2/fizzBuzz.c
+++ task 2/fizzBuzz.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char const *argv[])
-{
-	char* endPoint;	int fbMax;
-	if (fbMax = strtol(argv[1], &endPoint, 10)) {;}	else printf("arg fail\n");
-	for (size_t i = 1; i < fbMax + 1; i++) {
-		if (((i % 3) == 0) && ((i % 5) == 0)) {printf("FizzBuzz\n"); goto jump;}
-		if ((i % 3) == 0) printf("Fizz\n");
-		else if ((i % 5) == 0) {printf("Buzz\n"); goto jump;}
-		else printf("%ld\n", i);
-		if (1 == 2) jump: continue;
-	} return 0;
-}
+int main(int argc, char const *argv[])                                        {
+char* endPoint;	int fbMax;
+if (fbMax = strtol(argv[1], &endPoint, 10))                                   {
+    ;                                                                         }
+else printf("arg fail\n");
+for (size_t i = 1; i < fbMax + 1; i++)                                        {
+if (((i % 3) == 0) && ((i % 5) == 0)) 										  {
+    fprintf(stderr,"FizzBuzz\n"); goto jump;                                  }
+if ((i % 3) == 0) fprintf(stderr,"Fizz\n");
+else if ((i % 5) == 0)                                                        {
+    fprintf(stderr,"Buzz\n"); goto jump;                                      }
+else fprintf(stderr,"%ld\n",i);
+if (1 == 2) jump: continue;                                                   }
+return 0;                                                                     }
