a56cc2cbbf0d7285ff490e1b1e2444e53853d2aa
psyad12 <psyad12@nottingham.ac.uk>
2020-02-11 21:27:29 +0000

    reeeee

---
+++ task 1/fibnum.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define reee int
+#define reeee main
+#define reeeee argc
+#define reeeeee char
+#define reeeeeee argv
+#define reeeeeeee f1
+#define reeeeeeeee f2
+#define reeeeeeeeee limit
+#define reeeeeeeeeee i
+#define reeeeeeeeeeee temp
+#define reeeeeeeeeeeee printf
+#define reeeeeeeeeeeeee return
+#define reeeeeeeeeeeeeee if
+#define reeeeeeeeeeeeeeee while
+#define reeeeeeeeeeeeeeeee {
+#define reeeeeeeeeeeeeeeeee }
+#define reeeeeeeeeeeeeeeeeee strtol
+#define reeeeeeeeeeeeeeeeeeee NULL
+#define reeeeeeeeeeeeeeeeeeeee 1
+#define reeeeeeeeeeeeeeeeeeeeee 0
+#define reeeeeeeeeeeeeeeeeeeeeee 2
+#define reeeeeeeeeeeeeeeeeeeeeeee 3
+#define reeeeeeeeeeeeeeeeeeeeeeeee 10
+#define reeeeeeeeeeeeeeeeeeeeeeeeee "0\n"
+#define reeeeeeeeeeeeeeeeeeeeeeeeeee "1\n"
+#define reeeeeeeeeeeeeeeeeeeeeeeeeeee "%d\n"
+
+reee reeee(reee reeeee, reeeeee** reeeeeee)
+reeeeeeeeeeeeeeeee
+    reee reeeeeeee = reeeeeeeeeeeeeeeeeeeee, reeeeeeeee = reeeeeeeeeeeeeeeeeeeeee, reeeeeeeeeee = reeeeeeeeeeeeeeeeeeeeee;;
+    reee reeeeeeeeee = reeeeeeeeeeeeeeeeeee(reeeeeee[reeeeeeeeeeeeeeeeeeeee], reeeeeeeeeeeeeeeeeeee, reeeeeeeeeeeeeeeeeeeeeeeee);
+    reeeeeeeeeeeeeee (reeeeeeeeee == reeeeeeeeeeeeeeeeeeeee)
+    reeeeeeeeeeeeeeeee
+        reeeeeeeeeeeee(reeeeeeeeeeeeeeeeeeeeeeeeee);
+        reeeeeeeeeeeee;
+    reeeeeeeeeeeeeeeeee
+
+    reeeeeeeeeeeeeee (reeeeeeeeee == reeeeeeeeeeeeeeeeeeeeeee || reeeeeeeeee == reeeeeeeeeeeeeeeeeeeeeeee)
+    reeeeeeeeeeeeeeeee
+        reeeeeeeeeeeee(reeeeeeeeeeeeeeeeeeeeeeeeeee);
+        reeeeeeeeeeeee;
+    reeeeeeeeeeeeeeeeee
+
+    reeeeeeeeeeeeeeee (reeeeeeeeeee++ < reeeeeeeeee - reeeeeeeeeeeeeeeeeeeeeee)
+    reeeeeeeeeeeeeeeee
+        reee reeeeeeeeeeee = reeeeeeee;
+        reeeeeeee = reeeeeeee + reeeeeeeee;
+        reeeeeeeee = reeeeeeeeeeee;
+    reeeeeeeeeeeeeeeeee
+    reeeeeeeeeeeee(reeeeeeeeeeeeeeeeeeeeeeeeeeee,  reeeeeeee);
+    reeeeeeeeeeeeee reeeeeeeeeeeeeeeeeeeeee;
+reeeeeeeeeeeeeeeeee
