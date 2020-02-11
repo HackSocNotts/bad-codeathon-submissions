bea1dc8fe15bd6162ed44a0c139518f63a03e8c3
Samuel King <49367123+psysk6@users.noreply.github.com>
2020-02-11 22:50:47 +0000

    god is dead

---
+++ fib.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#define 😀 int
+#define 😁 main
+#define 😂 argc
+#define 🤣 char
+#define 😃 argv
+#define 😄 out
+#define 🤑 count
+#define 🤡 toLookUp
+#define 🍆 inFile
+#define  🐍 if
+#define 🔥 = 
+#define  💦
+#define  🐓 while
+#define 👩‍💻 open
+#define 🤕 cout 
+#define 🤮 exit 
+#define 😏 ifstream 
+#define 🤠 "lookup.txt"
+#define 🤯 "Unable to open file"
+#define 🤪 <<
+#define 🤘 >>
+#define 👀 0
+#define 😮 else
+#define 🦷 ++
+#define 😷 1
+#define 🕴 printf
+#define 👪 close
+#define 😤 ()
+#define 😖 return 
+#define 💅 ;
+#define 💁🏼 "%d\n"
+#define 💋 !
+using namespace std 💅 
+
+😀 😁(😀 😂, 🤣 *😃[]) {
+    😀 😄 💅
+    😀 🤑 🔥 👀 💅 
+    😀 🤡 🔥 10 💅 
+    😏 🍆 💅 
+    
+    🍆.👩‍💻(🤠) 💅 
+     🐍  ( 💋 🍆) {
+        🤕 🤪 🤯 💅 
+        🤮(😷) 💅 
+    }
+    
+    🐓(🍆 🤘 😄) {
+         🐍 (🤑 == 🤡){
+            🕴(💁🏼,😄) 💅 
+            🤮(👀) 💅 
+        }
+        😮{
+            🤑 🦷 💅 
+        }
+    }
+    
+    🍆.👪 😤 💅
+    😖 👀 💅
+}
