133c9ba420c326def4dbbfbb911f8f7e42842be1
Jan-Sen <jansencheng3@gmail.com>
2020-02-11 20:15:50 +0000

    Add files via upload

--- BadCode/FizzBuzz.py
+++ BadCode/FizzBuzz.py
@@ -1,12 +1,11 @@
 print("Enter the number")
 num=input()
-j=int(num)
 i=int(int(num)/int(num))
-while i<j:
+while i<int(num)+1:
     if i%((i/i)+(i/i)+(i/i))==i-i:
         if i%((i/i)+(i/i)+(i/i)+(i/i)+(i/i))==i-i:print("Fizz\nBuzz")
         else:print("Fizz ")
     else:
-        if i%((i/i)+(i/i)+(i/i))==i-i:print("Buzz")
+        if i%((i/i)+(i/i)+(i/i)+(i/i)+(i/i))==i-i:print("Buzz")
         else:print(i)
     i=i+(i-(i-1))
