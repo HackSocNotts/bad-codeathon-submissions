623f81cfb3a9bc7bfdefa94028255d388e229530
Jan-Sen <jansencheng3@gmail.com>
2020-02-11 21:00:15 +0000

    Add files via upload

--- BadCode/FizzBuzz.py
+++ BadCode/FizzBuzz.py
@@ -4,8 +4,8 @@ i=int(int(num)/int(num))
 while i<int(num)+int(i/i):
     if i%((i/i)+(i/i)+(i/i))==i-i:
         if i%((i/i)+(i/i)+(i/i)+(i/i)+(i/i))==i-i:print("Fizz\nBuzz")
-        else:print("Fizz ")
+        else:print("Fizz")
     else:
         if i%((i/i)+(i/i)+(i/i)+(i/i)+(i/i))==i-i:print("Buzz")
         else:print(i)
-    i=i+(i-(i-1))
+    i=i+(i-(i-int(i/i)))
