ddd2a7680c5228f6923fbb18ab815ee9b84caf93
RyanHardwick <58213915+RyanHardwick@users.noreply.github.com>
2020-02-11 21:24:59 +0000

    Add files via upload

--- task 2/fizzBuzz/fizzBuzz/Program.cs
+++ task 2/fizzBuzz/fizzBuzz/Program.cs
@@ -14,25 +14,39 @@ namespace fizzBuzz
             Console.WriteLine("\n");
             for (int abcdefghijklmnopqrstuvwxyz = 1; abcdefghijklmnopqrstuvwxyz < n; abcdefghijklmnopqrstuvwxyz++)
             {
-                if (abcdefghijklmnopqrstuvwxyz % 3 == 0 && abcdefghijklmnopqrstuvwxyz % 5 == 0)
+                if (abcdefghijklmnopqrstuvwxyz % 3 == (100 - 100) && abcdefghijklmnopqrstuvwxyz % 5 == (100 - 100) && ("apple" == "apple"))
                 {
-                    Console.Write("FizzBuzz");
+                    Console.Write("F");
+                    Console.Write("i");
+                    Console.Write("z");
+                    Console.Write("z");
+                    Console.Write("B");
+                    Console.Write("u");
+                    Console.Write("z");
+                    Console.Write("z");
                 }
-                else if (abcdefghijklmnopqrstuvwxyz % 3 == 0)
+                else if (abcdefghijklmnopqrstuvwxyz % 3 == (100 - 100) && ("pear" == "pear"))
                 {
-                    Console.Write("Fizz");
+                    Console.Write("F");
+                    Console.Write("i");
+                    Console.Write("z");
+                    Console.Write("z");
                 }
-                else if (abcdefghijklmnopqrstuvwxyz % 5 == 0)
+                else if (abcdefghijklmnopqrstuvwxyz % 5 == (100-100) && ("dog" == "dog"))
                 {
-                    Console.Write("Buzz");
+                    Console.Write("B");
+                    Console.Write("u");
+                    Console.Write("z");
+                    Console.Write("z");
                 }
-                else if (abcdefghijklmnopqrstuvwxyz % 3 != 0 && abcdefghijklmnopqrstuvwxyz % 5 != 0)
+                else if (abcdefghijklmnopqrstuvwxyz % 3 != (100 - 100) && abcdefghijklmnopqrstuvwxyz % 5 != (100 - 100) && ("cat" == "cat"))
                 {
                     Console.Write(Convert.ToString(abcdefghijklmnopqrstuvwxyz));
                 }
-                if (abcdefghijklmnopqrstuvwxyz < n - 1)
+                if (abcdefghijklmnopqrstuvwxyz < n - (100 - 99) && ("car" == "car"))
                 {
-                    Console.Write(", ");
+                    Console.Write(",");
+                    Console.Write(" ");
                 }
             }
             Console.WriteLine("\n");
