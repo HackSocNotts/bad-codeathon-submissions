9cd170a6b077e800d4de49588149516ac21eaef9
RyanHardwick <58213915+RyanHardwick@users.noreply.github.com>
2020-02-11 21:22:52 +0000

    Add files via upload

--- task 1/Fibonacci/Fibonacci/Program.cs
+++ task 1/Fibonacci/Fibonacci/Program.cs
@@ -6,20 +6,34 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            iccanobif(1);
-            iccanobif(6);
-            iccanobif(13);
+            iccanobif(1);iccanobif(6);iccanobif(13);
         }
 
         static void iccanobif(int temp4)
         {
             int temp2 = 1;int temp3 = 2;int temp5 = 0;int temp = 0;
 
-            for(int i = 0; i < temp4 - 4; i++)
+            for(int i = 0; i < temp4 - (8 - 4); i = i + (1+1+1+1+1+1+1-6)){temp = temp2 + temp3;temp2 = temp3;temp3 = temp;temp5 = temp;}
+            if(1==1)
             {
-                temp = temp2 + temp3;temp2 = temp3;temp3 = temp;temp5 = temp;
+                if(7==8)
+                {
+
+                }
+                else
+                {
+                    Console.Write("f");
+                    Console.Write("i");
+                    Console.Write("b");
+                    Console.Write("(");
+                    Console.Write(temp4);
+                    Console.Write(")");
+                    Console.Write(":");
+                    Console.Write(" ");
+                    Console.Write(temp5);
+                    Console.Write("\n");
+                }
             }
-            Console.WriteLine("fib(" + temp4 + "): " + temp5);
         }
     }
 }
