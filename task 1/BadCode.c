9109cbd8e4af52bebea214353351beb3fff3c6e8
psylc9 <60942009+psylc9@users.noreply.github.com>
2020-02-11 21:27:28 +0000

    Add files via upload

---
+++ task 1/BadCode.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int Main;
+int mian = 1;
+int Hackermusic_starts_playing(int);
+int Entering_your_zipcode(int);
+int Im_In(int);
+
+int main(int argc, char **argv)
+{
+    char *AngryMan1;
+    int AngryWomen;
+    int voom;
+    int zoom;
+    int toom;
+    mian = mian + 80;
+    mian = (mian/9)/9;
+
+    zoom = 0;
+
+    AngryWomen = strtol(argv[1], &AngryMan1, 10);
+    Main = AngryWomen;
+
+    voom = 0;
+
+    while(Main)
+    {
+                    AngryWomen=voom+zoom;
+            goto Gopper;gopper:
+                        toom = AngryWomen;AngryWomen = zoom;zoom = toom;voom = AngryWomen;goto hopper;Gopper:goto gopper;hopper:
+            if(zoom == 0){zoom = zoom + 1;AngryWomen = voom + zoom;}
+    Main = Hackermusic_starts_playing(Main); 
+    }
+    if(AngryWomen > voom){voom = AngryWomen;}
+                printf("%d\n", AngryWomen);
+
+    return 0;
+}
+/*BLACK SCREEN                                                
+MAIN TITLES
+IT'S SILENT. A BEAT...AND AN EXPLOSION OF SOUND.  A HOUSEHOLD
+IN THE MORNING. KIDS GETTING READY FOR SCHOOL. CLOCK RADIOS.
+KITCHEN APPLIANCES. SHOWERS. FIGHTING. PEOPLE YELLING. DOG
+BARKING. APPLIANCES BUZZING. CAR HORNS. IT SOUNDS JUST LIKE
+YOUR HOUSE DID. STREAMS OF ROCK'N ROLL FADE IN AND OUT. HUEY
+LEWIS TO LIONEL RITCHIE TO HUSKER DU. SURROUND MAKES IT FEEL
+LIKE YOU'RE IN THE ROOM. AN AURAL TOUR OF A HOUSE ON A SCHOOL
+MORNING. BEGINING IN THE KITCHEN AND MOVING UPSTAIRS.
+FATHER'S VOICE (TOM)
+Where's my wallet?!
+SEVEN YEAR OLD BOY (TODD)
+YOU IDIOT!!
+TWELVE YEAR OLD GIRL (KIMBERLY)*/
+
+
+int Hackermusic_starts_playing(int Naim)
+{return Entering_your_zipcode(Naim);}
+int Entering_your_zipcode(int IamN)
+{return Im_In(IamN);}
+int Im_In(int Yurg)
+{int mian_;
+            mian_ = mian;
+    Yurg = Yurg - mian_;
+return Yurg;}
