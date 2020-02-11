d8583447cca0327f50ee2de556cc4edf3afcf7f5
RyanHardwick <58213915+RyanHardwick@users.noreply.github.com>
2020-02-11 21:05:44 +0000

    Add files via upload

---
+++ task 2/fizzBuzz/fizzBuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace fizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            zzuBzzif(19);
+        }
+
+        static void zzuBzzif(int n)
+        {
+            Console.WriteLine("\n");
+            for (int abcdefghijklmnopqrstuvwxyz = 1; abcdefghijklmnopqrstuvwxyz < n; abcdefghijklmnopqrstuvwxyz++)
+            {
+                if (abcdefghijklmnopqrstuvwxyz % 3 == 0 && abcdefghijklmnopqrstuvwxyz % 5 == 0)
+                {
+                    Console.Write("FizzBuzz");
+                }
+                else if (abcdefghijklmnopqrstuvwxyz % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                else if (abcdefghijklmnopqrstuvwxyz % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                else if (abcdefghijklmnopqrstuvwxyz % 3 != 0 && abcdefghijklmnopqrstuvwxyz % 5 != 0)
+                {
+                    Console.Write(Convert.ToString(abcdefghijklmnopqrstuvwxyz));
+                }
+                if (abcdefghijklmnopqrstuvwxyz < n - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
