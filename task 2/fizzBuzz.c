39b6be15ba85e12df4854e63e34a181c30f68575
Maximilian Zielonka <psymz@nottingham.ac.uk>
2020-02-11 21:29:32 +0000

    its cool now

--- task 2/fizzBuzz.c
+++ task 2/fizzBuzz.c
@@ -6,14 +6,14 @@ int main(int argc, char const *argv[])                                        {
 char* endPoint;	int fbMax;
 if (fbMax = strtol(argv[1], &endPoint, 10))                                   {
     ;                                                                         }
-else writeln(stderr, "arg fail\n");
+else writeln(stderr, "👎\n");
 for (size_t i = 1; i < fbMax + 1; i++)                                        {
 if (((i % 3) == 0) && ((i % 5) == 0)) 										  {
-    writeln(stderr,"FizzBuzz\n"); jump here;                                  }
-if (((i % 3) == 0) && 1) writeln(stderr,"Fizz\n");
+    writeln(stderr,"👉 FizzBuzz 👈\n"); jump here;                            }
+if (((i % 3) == 0) && 1) writeln(stderr,"👉 Fizz 👈\n");
 else if (((i % 5) == 0) && 1)                                                 {
-    writeln(stderr,"Buzz\n"); jump here;                                      }
-else writeln(stderr,"%ld\n",i);
+    writeln(stderr,"👉 Buzz 👈\n"); jump here;                                }
+else writeln(stderr,"👉 %ld 👈\n",i);
 if (1 == 2) here: continue;
-writeln(stderr,"Press any key to continue");getchar();                        }
+writeln(stderr,"⌨️ Press any key to continue ⌨️");getchar();                  }
 return 0;                                                                     }
