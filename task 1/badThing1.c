6759e3c15f2072c9e10566771b1b898c8385a392
TeFka <32374819+TeFka@users.noreply.github.com>
2020-02-11 21:28:17 +0000

    Add files via upload

---
+++ task 1/badThing1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){int
+AAaAAAaaa=0,AaaAaaAAAA=1,AAAAaaaAaa=0,AaAaAaAAAAA;scanf("%d",&AaAaAaAAAAA);int i=3;if(
+AaAaAaAAAAA==1){AAAAaaaAaa=0;}else if(AaAaAaAAAAA==2){AAAAaaaAaa=1;
+}else{AaaA:AAaAAAaaa=AaaAaaAAAA;AaaAaaAAAA = AAAAaaaAaa;
+AAAAaaaAaa = AAaAAAaaa+AaaAaaAAAA;i++;if(i<
+AaAaAaAAAAA+2){goto AaaA;}}printf("%d",AAAAaaaAaa);return 0;}
