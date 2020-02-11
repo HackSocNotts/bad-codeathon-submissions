e5fe93d63eb391470e02960f6eaa92243e42a470
Maximilian Zielonka <psymz@nottingham.ac.uk>
2020-02-11 21:19:45 +0000

    even worserer

--- task 2/fizzBuzz.c
+++ task 2/fizzBuzz.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define writeln fprintf
+#define jump goto
 int main(int argc, char const *argv[])                                        {
 char* endPoint;	int fbMax;
 if (fbMax = strtol(argv[1], &endPoint, 10))                                   {
     ;                                                                         }
-else printf("arg fail\n");
+else writeln(stderr, "arg fail\n");
 for (size_t i = 1; i < fbMax + 1; i++)                                        {
 if (((i % 3) == 0) && ((i % 5) == 0)) 										  {
-    fprintf(stderr,"FizzBuzz\n"); goto jump;                                  }
-if (((i % 3) == 0) && 1) fprintf(stderr,"Fizz\n");
+    writeln(stderr,"FizzBuzz\n"); jump here;                                  }
+if (((i % 3) == 0) && 1) writeln(stderr,"Fizz\n");
 else if (((i % 5) == 0) && 1)                                                 {
-    fprintf(stderr,"Buzz\n"); goto jump;                                      }
-else fprintf(stderr,"%ld\n",i);
-if (1 == 2) jump: continue;                                                   }
+    writeln(stderr,"Buzz\n"); jump here;                                      }
+else writeln(stderr,"%ld\n",i);
+if (1 == 2) here: continue;
+writeln(stderr,"Press any key to continue");getchar();                       }
 return 0;                                                                     }
