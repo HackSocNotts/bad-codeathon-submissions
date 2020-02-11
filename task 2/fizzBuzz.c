aea3443ac302341e5a1cae2bd946cfbaab779e4c
Maximilian Zielonka <psymz@nottingham.ac.uk>
2020-02-11 21:07:58 +0000

    worserer

--- task 2/fizzBuzz.c
+++ task 2/fizzBuzz.c
@@ -9,8 +9,8 @@ else printf("arg fail\n");
 for (size_t i = 1; i < fbMax + 1; i++)                                        {
 if (((i % 3) == 0) && ((i % 5) == 0)) 										  {
     fprintf(stderr,"FizzBuzz\n"); goto jump;                                  }
-if ((i % 3) == 0) fprintf(stderr,"Fizz\n");
-else if ((i % 5) == 0)                                                        {
+if (((i % 3) == 0) && 1) fprintf(stderr,"Fizz\n");
+else if (((i % 5) == 0) && 1)                                                 {
     fprintf(stderr,"Buzz\n"); goto jump;                                      }
 else fprintf(stderr,"%ld\n",i);
 if (1 == 2) jump: continue;                                                   }
